de9b44d52eace1b14815f5317b511ec9ba1d4032
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-02-01 12:21:30 -0500

    simplify height animation

--- src/components/Definition.tsx
+++ src/components/Definition.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useRef } from 'react';
-
 import { Text } from '@/components/Text';
 import { useDictionaryApi } from '@/hooks/useDictionaryApi';
 
@@ -18,53 +16,37 @@ export function Definition({
     word,
     onError,
   });
-  const ref = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (!ref.current) return;
-    const height = ref.current.offsetHeight.valueOf();
-    if (height === 0) return;
-
-    document.documentElement.style.setProperty(
-      '--target-height',
-      `${height}px`,
-    );
-    ref.current?.classList.add('animated');
-  }, [ref.current, ref.current?.offsetHeight]);
-
-  if (!win) return null;
   return (
     <div className="word-data">
-      {data ? (
-        <div className="expandable" ref={ref}>
-          <Text description={data?.phonetic} textClass="phonetic" />
-
+      {data && win ? (
+        <>
+          <Text description={data?.phonetic} variant="phonetic" />
           {data.meanings.map((meaning, mIndex) => (
             <div className="definitions" key={mIndex}>
-              {meaning.partOfSpeech ? (
-                <p>
-                  <i>{meaning.partOfSpeech}</i>
-                </p>
-              ) : null}
+              <Text
+                description={meaning.partOfSpeech}
+                variant="part-of-speech"
+              />
               <ul>
                 {meaning.definitions.map((definition, dIndex) => (
                   <li key={dIndex}>
                     <Text description={definition.definition} />
                     <Text description={definition.example} label="Example" />
                     {/* <Text
-                description={definition.synonyms.join(', ')}
-                label="Synonyms"
-              /> */}
+                          description={definition.synonyms.join(', ')}
+                          label="Synonyms"
+                        /> */}
                     {/* <Text
-                description={definition.antonyms.join(', ')}
-                label="Antonyms"
-              /> */}
+                          description={definition.antonyms.join(', ')}
+                          label="Antonyms"
+                        /> */}
                   </li>
                 ))}
               </ul>
             </div>
           ))}
-        </div>
+        </>
       ) : null}
     </div>
   );
