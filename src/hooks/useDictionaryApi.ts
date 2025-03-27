32abf6a35957fb5090f8034b30429a665f50a3c1
Kelly Ripple <kelly.m.ripple@gmail.com>
2025-03-27 17:26:19 -0400

    use better dictionary api

--- src/hooks/useDictionaryApi.ts
+++ src/hooks/useDictionaryApi.ts
@@ -1,22 +1,76 @@
 import { useEffect, useState } from 'react';
 
-export type DictionaryData = {
-  word: string;
-  phonetic: string;
-  phonetics: {
-    text: string;
-    audio?: string;
-  }[];
-  origin: string;
-  meanings: {
-    partOfSpeech: string;
-    definitions: {
-      definition: string;
-      example: string;
-      synonyms: string[];
-      antonyms: string[];
+type DictionaryEntry = {
+  meta: {
+    id: string;
+    uuid: string;
+    sort: string;
+    src: string;
+    section: string;
+    stems: string[];
+    offensive: boolean;
+  };
+  hwi: {
+    hw: string;
+    prs: {
+      mw: string;
+      sound?: {
+        audio: string;
+        ref: string;
+        stat: string;
+      };
     }[];
+  };
+  fl: string;
+  def: {
+    sseq: [
+      [
+        [
+          'sense',
+          {
+            sn: string;
+            dt: [string, { t: string }[]][];
+            sdsense?: {
+              sd: string;
+              dt: [string, { t: string }[]][];
+            };
+          },
+        ],
+      ],
+    ][];
   }[];
+  uros?: {
+    ure: string;
+    fl: string;
+  }[];
+  et: [string, string][];
+  date: string;
+  ld_link: {
+    link_hw: string;
+    link_fl: string;
+  };
+  suppl?: {
+    examples: { t: string }[];
+    ldq: {
+      ldhw: string;
+      fl: string;
+      def: {
+        sls?: string[];
+        sseq: [
+          [
+            [
+              'sense',
+              {
+                dt: [string, string][];
+                sls?: string[];
+              },
+            ],
+          ],
+        ][];
+      }[];
+    };
+  };
+  shortdef: string[];
 };
 
 export const useDictionaryApi = ({
@@ -24,16 +78,16 @@ export const useDictionaryApi = ({
   onError,
 }: {
   word: string;
-  onError: () => void;
+  onError?: () => void;
 }) => {
-  const [data, setData] = useState<DictionaryData>();
+  const [data, setData] = useState<DictionaryEntry>();
 
   useEffect(() => {
-    const baseUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/' as const;
-    fetch(`${baseUrl}${word}`)
+    const baseUrl = 'https://api.kellyripple.com/dictionary' as const;
+    fetch(`${baseUrl}/${word}`)
       .then((response) => {
         if (response.status === 404) {
-          return onError();
+          return onError?.();
         }
         if (response.status !== 200) {
           console.debug(`unexpected response code '${response.status}'`);
@@ -46,6 +100,7 @@ export const useDictionaryApi = ({
       })
       .catch((error) => {
         console.warn(error);
+        onError?.();
       });
   }, [word]);
 
